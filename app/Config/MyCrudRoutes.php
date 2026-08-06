550602bd55b6b2bceb22787e80a537b93116fb09
office <office@office>
2026-08-06 10:45:30 +0200

    myCrudGpt 2.6.1

--- app/Config/MyCrudRoutes.php
+++ app/Config/MyCrudRoutes.php
@@ -2,20 +2,72 @@
 
 use CodeIgniter\Router\RouteCollection;
 
-/** @var RouteCollection $routes */
-$routes->group('mycrud', ['namespace' => 'App\Controllers\MyCrud'], static function (RouteCollection $routes): void {
-    $routes->get('/', 'BuilderController::index');
 
-    $routes->get('auto/(:segment)', 'AutoCrudController::generate/$1');
 
-    $routes->get('builder/configure/(:segment)', 'BuilderController::configure/$1');
-    $routes->post('builder/save', 'BuilderController::save');
-    $routes->post('builder/generate', 'BuilderController::generate');
+$routes->group(
+    'mycrud',
+    ['namespace' => 'App\Controllers\MyCrud'],
+    static function (RouteCollection $routes): void {
+        /*
+         * Home / Builder.
+         */
+        $routes->get('/', 'BuilderController::index');
+        $routes->get('builder', 'BuilderController::index');
 
-    $routes->get('schema', 'SchemaController::index');
-    $routes->get('schema/(:segment)', 'SchemaController::index/$1');
-    $routes->get('tools/routes', 'ToolsController::routes');
-    $routes->get('tools/fields', 'ToolsController::fields');
-    $routes->get('tools/schema', 'ToolsController::schema');
-    $routes->get('tools/schema/(:segment)', 'ToolsController::schema/$1');
-});
+        $routes->get(
+            'builder/configure/(:segment)',
+            'BuilderController::configure/$1'
+        );
+
+        $routes->post(
+            'builder/save',
+            'BuilderController::save'
+        );
+
+        $routes->post(
+            'builder/generate',
+            'BuilderController::generate'
+        );
+
+        /*
+         * Quick globale.
+         */
+        $routes->get(
+            'quick',
+            'AutoCrudController::index'
+        );
+
+        $routes->post(
+            'quick/generate',
+            'AutoCrudController::generateAll'
+        );
+
+        $routes->get(
+            'quick/report/(:segment)',
+            'AutoCrudController::report/$1'
+        );
+
+        /*
+         * Strumenti.
+         */
+        $routes->get(
+            'tools/routes',
+            'ToolsController::routes'
+        );
+
+        $routes->get(
+            'tools/fields',
+            'ToolsController::fields'
+        );
+
+        $routes->get(
+            'tools/schema',
+            'ToolsController::schema'
+        );
+
+        $routes->get(
+            'tools/schema/(:segment)',
+            'ToolsController::schema/$1'
+        );
+    }
+);
